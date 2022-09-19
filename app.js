6a010b4e25d753f3afc14648e2a6b89dedfdccfb
ivan2798 <inge1998@hotmail.com>
2022-09-19 13:12:39 -0500

    funciones declaradas vs anónimo

--- app.js
+++ app.js
@@ -22,8 +22,27 @@ function unaFuncionQueDevuelveValor(){
 
 //console.log(valorDeFuncion);
 
-function saludar(nombre,pais){
+function saludar(nombre="...",pais="..."){
     console.log(`Mi nombre es: ${ nombre } y mi vivo en ${ pais }`);
 }
 
-saludar("Iván","México")
+saludar("Iván","México");
+saludar();
+
+//funciones declaradas VS funciones expresadas
+
+funcionDeclarada();
+
+function funcionDeclarada(){
+    console.log("Esto puede clararse en cualquier parte de nuestro código,incluso antes de que la función sea declarada");
+}
+
+//funcionDeclarada();
+
+//funcion anónima
+funcionExpresada();
+const funcionExpresada = function(){
+      console.log("Esta es una función expresada, es decir, una función que se le ha asignado como valor a una variable, si invocamos esta funcion antes de su declaración en JS nos dirá");
+}
+
+funcionExpresada();
